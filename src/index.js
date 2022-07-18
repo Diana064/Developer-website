8d84f8d40323a9ff02fa5fba3b88e94c3a8ce3c4
Diana064 <dipustolyakova02@gmail.com>
2022-07-18 22:25:58 +0300

    delete some

--- src/index.js
+++ src/index.js
@@ -71,3 +71,39 @@ var slider2 = new Swiper('.slider2', {
     mobileMenuRef.classList.toggle('is-open');
   });
 })();
+
+function formValidation() {
+  var uid = document.registration.userid;
+  var uname = document.registration.username;
+
+  var uemail = document.registration.email;
+
+  if (userid_validation(uid, 5, 12)) {
+    if (passid_validation(passid, 7, 12)) {
+      if (allLetter(uname)) {
+        if (countryselect(ucountry)) {
+          if (ValidateEmail(uemail)) {
+          }
+        }
+      }
+    }
+  }
+  return false;
+}
+//validating email
+function ValidateEmail(input) {
+  var validRegex =
+    /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+  if (input.value.match(validRegex)) {
+    document.form1.text1.focus();
+
+    return true;
+  } else {
+    alert('Invalid email address!');
+
+    document.form1.text1.focus();
+
+    return false;
+  }
+}
